c8610051fb8a9649d93cb7d43756cc1885b68acd
OpaTio <natadsouza4@gmail.com>
2022-04-28 11:30:51 -0300

    1.0.0

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+var somar = require("./modulos/somar");
+var subtracao = require("./modulos/subtracao");
+var divisao = require("./modulos/divisao");
+var multiplicao = require("./modulos/multiplicao");
+
+console.log(multiplicao(10,5));
+console.log(divisao(50,5));
+console.log(subtracao(100,50));
+console.log(somar(200,300));
